4408175f14f5f885cea3c1315ea990b85cea7701
Shiwangsh KC <Shiwangshkc@gmail.com>
2023-02-10 10:47:58 +0545

    random user follow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,34 @@ import Widgets from "@/components/Widgets";
 
 export default async function Home() {
   const newsArticle = await getNews();
+  const randomUsers = await getRandomUsers();
   return (
     <main className="flex min-h-screen mx-auto">
       <Sidebar />
       <Feed />
-      <Widgets newsArticle={newsArticle.articles} />
+      <Widgets
+        newsArticle={newsArticle.articles}
+        randomUsers={randomUsers.results}
+      />
     </main>
   );
 }
 
-export async function getNews() {
+async function getNews() {
   const res = await fetch(
     "https://saurav.tech/NewsAPI/top-headlines/category/business/us.json"
+  );
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+async function getRandomUsers() {
+  const res = await fetch(
+    "https://randomuser.me/api/?results=30&inc=name,login,picture"
   );
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
