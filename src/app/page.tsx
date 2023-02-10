efb9ff8930e5cddf308e6753da054b3ecfbb9e11
Shiwangsh KC <Shiwangshkc@gmail.com>
2023-02-10 10:18:55 +0545

    Feed and news section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
 import Feed from "@/components/Feed";
 import Sidebar from "@/components/Sidebar";
+import Widgets from "@/components/Widgets";
 
-export default function Home() {
+export default async function Home() {
+  const newsArticle = await getNews();
   return (
-    <main className="flex min-h-screen max-w-7xl mx-auto">
+    <main className="flex min-h-screen mx-auto">
       <Sidebar />
       <Feed />
+      <Widgets newsArticle={newsArticle.articles} />
     </main>
   );
+}
+
+export async function getNews() {
+  const res = await fetch(
+    "https://saurav.tech/NewsAPI/top-headlines/category/business/us.json"
+  );
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
 }
