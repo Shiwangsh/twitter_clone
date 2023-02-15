85214d742ff6f8bf22d80d5e4df327867937ba9e
Shiwangsh KC <Shiwangshkc@gmail.com>
2023-02-15 10:26:44 +0545

    Show posts in feed and like functionality

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -7,15 +7,56 @@ import {
   ShareIcon,
   TrashIcon,
 } from "@heroicons/react/outline";
-import Image from "next/image";
-
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  setDoc,
+} from "firebase/firestore";
+import { db } from "../../firebase";
+import { signIn, useSession } from "next-auth/react";
+import { useState, useEffect } from "react";
+import Moment from "react-moment";
 export default function Post({ post }: any) {
+  const { data: session } = useSession();
+  const [likes, setLikes] = useState<any>([]);
+  const [hasLiked, setHasLiked] = useState<boolean>(false);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection(db, "posts", post.id, "likes"),
+      (snapshot) => setLikes(snapshot.docs)
+    );
+  }, [post.id]);
+
+  useEffect(() => {
+    setHasLiked(
+      likes.findIndex((like: any) => like.id === session?.user.uid) !== -1
+    );
+  }, [likes, session?.user.uid]);
+
+  async function likePost() {
+    if (session) {
+      if (hasLiked) {
+        await deleteDoc(doc(db, "posts", post.id, "likes", session?.user.uid));
+      } else {
+        await setDoc(doc(db, "posts", post.id, "likes", session?.user.uid), {
+          username: session.user.username,
+        });
+      }
+    } else {
+      signIn();
+    }
+  }
+
   return (
     <div className="flex p-3 cursor-pointer border-b border-gray-200">
       {/* user image */}
-      <Image
+      <img
         className="h-11 w-11 rounded-full mr-4"
-        src={post.userImg}
+        src={post.data().userImg}
         alt="user-img"
       />
       {/* right side */}
@@ -26,11 +67,13 @@ export default function Post({ post }: any) {
           {/* post user info */}
           <div className="flex items-center space-x-1 whitespace-nowrap">
             <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
-              {post.name}
+              {post.data().name}
             </h4>
-            <span className="text-sm sm:text-[15px]">@{post.username} - </span>
+            <span className="text-sm sm:text-[15px]">
+              @{post.data().username}
+            </span>
             <span className="text-sm sm:text-[15px] hover:underline">
-              {post.timestamp}
+              <Moment fromNow>{post?.timestamp?.toDate()}</Moment>
             </span>
           </div>
 
@@ -41,19 +84,41 @@ export default function Post({ post }: any) {
         {/* post text */}
 
         <p className="text-gray-800 text-[15px sm:text-[16px] mb-2">
-          {post.text}
+          {post.data().text}
         </p>
 
         {/* post image */}
 
-        <img className="rounded-2xl mr-2" src={post.img} alt="" />
+        <img className="rounded-2xl mr-2" src={post.data().image} alt="" />
 
         {/* icons */}
 
         <div className="flex justify-between text-gray-500 p-2">
           <ChatIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
           <TrashIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
-          <HeartIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
+
+          <div className="flex items-center">
+            {hasLiked ? (
+              <HeartIconFilled
+                onClick={likePost}
+                className="h-9 w-9 hoverEffect p-2 text-red-600 hover:bg-red-100"
+              />
+            ) : (
+              <HeartIcon
+                onClick={likePost}
+                className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100"
+              />
+            )}
+            {likes.length > 0 && (
+              <span
+                className={`${hasLiked && "text-red-600"} text-sm select-none`}
+              >
+                {" "}
+                {likes.length}
+              </span>
+            )}
+          </div>
+
           <ShareIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
           <ChartBarIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
         </div>
